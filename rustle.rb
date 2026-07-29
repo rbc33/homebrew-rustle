69a37719e365afe696221b77878687d5b3bf5432
rbc33 <ricardobenthem@gmail.com>
2026-07-29 17:55:07 +0200

    fix: typo

--- rustle.rb
+++ rustle.rb
@@ -1,9 +1,9 @@
-cask "Rustle" do
+cask "rustle" do
   version "1.0.0"
   sha256 "25d586157411ac5924dcfb3dbfb71e41dc5343a9d113863be9ca834e71aaeb78"
 
   url "https://github.com/rbc33/Rustle/releases/download/v#{version}/Rustle.dmg"
-  name "Rustl"
+  name "Rustle"
   desc "Adds satisfying paper texture to mac"
   homepage "https://github.com/rbc33/Rustle"
 
